16d336b13c4e033bb3022d768ef013c79417ce41
HuXiaoTu <13676373110@163.com>
2021-09-24 13:38:02 +0800

    完善注释介绍

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,17 @@ var path = require('path')
 var webpack = require('webpack')
 const NODE_ENV = process.env.NODE_ENV;
 module.exports = {
-  entry: NODE_ENV == 'development' ? './src/main.js' : './src/frameSelection/index.js',
+  // 打包入口
+  entry: NODE_ENV == 'development' ? './src/main.js' : './src/components/index.js',
+
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'FrameSelection.js',
-    library: 'FrameSelection', // 指定的就是你使用require时的模块名
-    libraryTarget: 'umd', // 指定输出格式
-    umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
+    filename: 'plugin.js',          // 输出文件名
+    library: 'plugin',              // 指定的就是你使用require时的模块名
+
+    libraryTarget: 'umd',           // 指定输出格式
+    umdNamedDefine: true            // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
   },
   module: {
     rules: [
